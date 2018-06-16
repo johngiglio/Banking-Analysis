b64234b868c3685ab6254efa992f237b84f9c337
John Giglio <johngiglio@gmail.com>
2018-06-16 18:03:24 -0400

    PyBank HW3 problem is solved.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,76 @@
-'read data
+# import packages
+import csv
+file = "budget_data.csv"
+
+
+# create lists to store date and revenue
+date = []
+revenue = []
+
+#read data
+with open(file,encoding="utf8") as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+# iterate through csvreader and append to date and revenue lists
+    header = next(csvreader) #skip headers
+    for row in csvreader:
+        date.append(row[0])
+        revenue.append(row[1])
+    # date.pop(0)
+    # revenue.pop(0)
+    # length of date list to get total number of months
+    nummonths = len(date)
+
+
+    # sum revenue
+    revsum = int()
+    # iterate to turn revenue into integer and add to sum
+    for i in range(0,len(revenue)):
+        revenue[i] = int(revenue[i])
+        revsum += revenue[i]
+    
+    
+    # average change in "Profit/Losses" between months over the entire period
+    # create new integer to store total monthly changes,  divide by nummonths - 1
+    # create new list to store monthly changes
+
+    totalchange = int()
+    monthchange = []
+    for i in range(len(revenue)-1):
+        totalchange += (revenue[i+1] - revenue[i])
+        monthchange.append((revenue[i+1] - revenue[i]))
+
+    avgchange = totalchange / (nummonths - 1)
+
+# 'print total months
+# 'print net profit/loss over entire period
+# 'print average change in profit/loss between months
+# 'print greatest increase in profits
+# 'print greatest decrease in losses
+
+    print("  Financial Analysis\n---------------------------- ")
+    print("Total Months: " + str(nummonths) )
+    print("Total: $" + str(totalchange))
+    print ("Average  Change: $" + str(avgchange))
+    print("Greatest Increase in Profits: " + date[monthchange.index(max(monthchange))] + " "+ str(max(monthchange)))
+    print("Greatest Decrease in Profits: " + date[monthchange.index(min(monthchange))] + " " + str(min(monthchange)))
 
 
 
 
-'print total months
-'print net profit/loss over entire period
-'print average change in profit/loss between months
-'print greatest increase in profits
-'print greatest decrease in losses
-'
+# write to csv
+output_file = "PyBank_solved.csv"
+
+with open(output_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    # Write the header row
+
+    writer.writerow(["  Financial Analysis"])
+    writer.writerow(["---------------------------- "])
+    writer.writerow(["Total Months: ",str(nummonths)])
+    writer.writerow(["Total:",  " $"+str(totalchange)])
+    writer.writerow(["Average  Change: $",str(avgchange)])
+    writer.writerow(["Greatest Increase in Profits: ",date[monthchange.index(max(monthchange))],str(max(monthchange))])
+    writer.writerow(["Greatest Decrease in Profits: ",date[monthchange.index(min(monthchange))],str(min(monthchange))])
---
+++ PyPoll/main.py
@@ -0,0 +1,81 @@
+# import packages
+
+import csv
+
+# read csv file
+# three columns: `Voter ID`, `County`, and `Candidate`
+# create empty lists to store data
+
+ID = []
+country = []
+candidate = []
+
+file = 'election_data.csv'
+
+with open(file,encoding="utf8") as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # iterate through csvreader and append to ID country and candidate lists, and pop out headers
+    header = next(csvreader) #skip headers
+   
+    for row in csvreader:
+        ID.append(row[0])
+        country.append(row[1])
+        candidate.append(row[2])
+
+    """  Election Results
+  -------------------------
+  Total Votes: 3521001
+  -------------------------
+  Khan: 63.000% (2218231)
+  Correy: 20.000% (704200)
+  Li: 14.000% (492940)
+  O'Tooley: 3.000% (105630)
+  -------------------------
+  Winner: Khan
+  -------------------------
+  ```"""
+    print('  Election Results\n-------------------------')
+#   * The total number of votes cast
+    totalvotes = len(ID)
+    print(f'Total Votes: {totalvotes}')
+    print('-------------------------')
+#   * A complete list of candidates who received votes
+    candidates_list = set(candidate)
+
+#   * The total number of votes each candidate won
+    votes = [candidate.count(name) for name in candidates_list]
+
+#   * The percentage of votes each candidate won
+    votes_perc = [100*num/sum(votes) for num in votes]
+
+    for cand, perc, vote in zip(candidates_list, votes_perc, votes):
+        print(f'{cand}: {perc:.3f}% ({vote})')
+
+#   * The winner of the election based on popular vote.
+    print('-------------------------')
+    winner = list(candidates_list)[votes.index(max(votes))]
+    print(f'Winner: {winner}')
+    print('-------------------------')
+
+    with open('election_results.csv', 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+        csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the rows
+        csvwriter.writerow(['  Election Results'])
+        csvwriter.writerow(['-------------------------'])
+        csvwriter.writerow(['Total Votes: ',str(totalvotes)])
+        csvwriter.writerow(['-------------------------'])
+
+        # iterate throughcandidates to print individual results
+        for cand, perc, vote in zip(candidates_list, votes_perc, votes):
+            csvwriter.writerow([str(cand) + ': ', f'{round(perc,3):.3f}%','="(' + str(vote) + ')"'])
+        
+        csvwriter.writerow(['-------------------------'])
+        csvwriter.writerow(['Winner :', str(winner)])
+        csvwriter.writerow(['-------------------------'])
+
+
